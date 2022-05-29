1a10bf28c155bc0c329077647d8b4876506f1308
_skm <7682435-_skm@users.noreply.gitlab.com>
2022-05-30 01:18:55 +0200

    add gaps for monocle layout

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int gappih    = 20;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const char *fonts[]          = { "monospace:size=18", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=18";
 static const char col_gray1[]       = "#222222";
